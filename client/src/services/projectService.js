59e04fc4f7aabd822ab84fbf04974bdcb1761e36
Thiago Luiz de Matos <127130121+thidematos@users.noreply.github.com>
2025-01-03 13:57:10 -0300

    stopped using Api

--- client/src/services/projectService.js
+++ client/src/services/projectService.js
@@ -5,7 +5,50 @@ const defaultUrl = "";
 export async function getAllProjects() {
   const res = await axios.get("/api/v1/projects");
 
-  return res.data.data.projects;
+  const projects = [
+    {
+      _id: "66ad3d7ecfb17f0a1671ed4d",
+      photos: ["photo-1722629502109-0.jpg"],
+      user: "669a949e11269717a4d8c0d8",
+      title: "Bancada",
+      material: "Granito Preto Escovado",
+      isMarmoraria: true,
+      createdAt: "1722629502125",
+      __v: "0",
+    },
+    {
+      _id: "66a9c1eba86159af73ece42a",
+      photos: ["photo-1722401259608-0.jpg"],
+      user: "669a949e11269717a4d8c0d8",
+      title: "Bancada e kit pia",
+      material: "Granito Branco Pitaya",
+      isMarmoraria: true,
+      createdAt: "1722401259632",
+      __v: "0",
+    },
+    {
+      _id: "669b0b14eb25d4a4d9a3c73c",
+      photos: ["photo-1721436948817-0.jpg"],
+      user: "669a949e11269717a4d8c0d8",
+      title: "Pergolado policarbonato",
+      material: "Alumínio Preto",
+      isMarmoraria: false,
+      createdAt: "1721436948865",
+      __v: "0",
+    },
+    {
+      _id: "669b0aeceb25d4a4d9a3c736",
+      photos: ["photo-1722405319473-0.jpg"],
+      user: "669a949e11269717a4d8c0d8",
+      title: "Portão",
+      material: "Alumínio Preto",
+      isMarmoraria: false,
+      createdAt: "1721436908130",
+      __v: "2",
+    },
+  ];
+
+  return projects;
 }
 
 export async function createProject(formProject) {
